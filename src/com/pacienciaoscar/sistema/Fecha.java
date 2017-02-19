6bac34df9300ad4bb7f94431005e0f447e238a0c
Neus <Neus@10.211.55.3>
2017-02-19 16:17:13 +0100

    3ª Refactorizacion

--- src/com/pacienciaoscar/sistema/Fecha.java
+++ src/com/pacienciaoscar/sistema/Fecha.java
@@ -4,15 +4,26 @@ public class Fecha {
 	private int dia; 
 	private int mes;
 	private int anio;
-public Fecha(int dia, int mes, int anio) {
-	this.dia = dia; this.mes = mes; this.anio = anio; 
+	
+	public Fecha(int dia, int mes, int anio) {
+	
+		this.dia = dia; 
+		this.mes = mes; 
+		this.anio = anio; 
 	}
+	
 	public boolean valida ( ) {
 		if (dia < 1 || dia > 31) return false; 
 		if (mes < 1 || mes > 12) return false; 
 		if (anio < 0) return false;
 		if(dia>diasMes())return false;
 		else return true;
+	}
+	
+	private boolean bisiesto(){
+		if ( (anio % 400 == 0) || ( (anio % 4 == 0) && (anio % 100 != 0) ))
+			return true;
+		else return false;
 	}
 		
 		private int diasMes(){
@@ -31,8 +42,8 @@ public Fecha(int dia, int mes, int anio) {
 		case 9:
 		case 11 : diasMes = 30;
 		break;
-		case 2 : 
-			if ( (anio % 400 == 0) || ( (anio % 4 == 0) && (anio % 100 != 0) ))
+		case 2 :
+			if (bisiesto())
 			diasMes = 29; 
 		else
 			diasMes = 28;
